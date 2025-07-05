9748b207469ab2827853b48f1eecb4aab01119f7
Savi-04 <shivangsomvanshi124@gmail.com>
2025-07-05 10:00:13 +0100

    multiple PIM and CPU integrated. Heuristic is also working

--- CloudSim_PIM_Scheduler_project/examples/org/cloudbus/cloudsim/examples/CloudSimExample1.java
+++ CloudSim_PIM_Scheduler_project/examples/org/cloudbus/cloudsim/examples/CloudSimExample1.java
@@ -16,86 +16,76 @@ public class CloudSimExample1 {
         Log.printLine("Starting CloudSimExample1...");
 
         try {
-            // Initialize CloudSim
+            // 1. Initialize CloudSim
             int num_user = 1;
             Calendar calendar = Calendar.getInstance();
             boolean trace_flag = false;
             CloudSim.init(num_user, calendar, trace_flag);
 
-            // Creating Datacenter with CPU and PIM hosts
+            // 2. Create Datacenter with multiple CPU and PIM hosts
             Datacenter datacenter0 = createDatacenter("Datacenter_0");
 
-            // Creating Broker
+            // 3. Create Broker
             DatacenterBroker broker = new DatacenterBroker("Broker");
             int brokerId = broker.getId();
 
-            // Creating VMs
+            // 4. Create VMs
             List<Vm> vmList = new ArrayList<>();
+            // Create 3 CPU VMs and 3 PIM VMs corresponding to the hosts
+            vmList.add(new Vm(0, brokerId, 10000, 1, 2048, 1000, 10000, "Xen", new CloudletSchedulerSpaceShared()));
+            vmList.add(new Vm(1, brokerId, 9000, 1, 2048, 1000, 10000, "Xen", new CloudletSchedulerSpaceShared()));
+            vmList.add(new Vm(2, brokerId, 11000, 1, 4096, 1000, 10000, "Xen", new CloudletSchedulerSpaceShared()));
 
-            Vm cpuVm = new Vm(0, brokerId, 10000, 1, 2048, 1000, 10000, "Xen", new CloudletSchedulerSpaceShared());
-            Vm pimVm = new Vm(1, brokerId, 8000, 1, 4096, 1000, 10000, "Xen", new CloudletSchedulerSpaceShared());
-            vmList.add(cpuVm);
-            vmList.add(pimVm);
+            vmList.add(new Vm(3, brokerId, 8000, 1, 4096, 1000, 10000, "Xen", new CloudletSchedulerSpaceShared()));
+            vmList.add(new Vm(4, brokerId, 7000, 1, 4096, 1000, 10000, "Xen", new CloudletSchedulerSpaceShared()));
+            vmList.add(new Vm(5, brokerId, 8500, 1, 4096, 1000, 10000, "Xen", new CloudletSchedulerSpaceShared()));
+
             broker.submitVmList(vmList);
 
-            // Creating Cloudlets
+            // 5. Create Cloudlets
             List<Cloudlet> cloudletList = new ArrayList<>();
             UtilizationModel utilizationModel = new UtilizationModelFull();
-
-            // RAM + Deadline for scheduling decision
             Map<Integer, Integer> ramMap = new HashMap<>();
             Map<Integer, Double> deadlineMap = new HashMap<>();
 
             Cloudlet cl0 = new Cloudlet(0, 10000, 1, 300, 300, utilizationModel, utilizationModel, utilizationModel);
-            cl0.setUserId(brokerId);
-            cloudletList.add(cl0);
-            ramMap.put(0, 2000);
-            deadlineMap.put(0, 60.0);
+            cl0.setUserId(brokerId); cloudletList.add(cl0); ramMap.put(0, 2000); deadlineMap.put(0, 60.0);
 
             Cloudlet cl1 = new Cloudlet(1, 400000, 1, 300, 300, utilizationModel, utilizationModel, utilizationModel);
-            cl1.setUserId(brokerId);
-            cloudletList.add(cl1);
-            ramMap.put(1, 1000);
-            deadlineMap.put(1, 25.0);
+            cl1.setUserId(brokerId); cloudletList.add(cl1); ramMap.put(1, 1000); deadlineMap.put(1, 25.0);
 
             Cloudlet cl2 = new Cloudlet(2, 8000, 1, 300, 300, utilizationModel, utilizationModel, utilizationModel);
-            cl2.setUserId(brokerId);
-            cloudletList.add(cl2);
-            ramMap.put(2, 256);
-            deadlineMap.put(2, 15.0);
+            cl2.setUserId(brokerId); cloudletList.add(cl2); ramMap.put(2, 256); deadlineMap.put(2, 15.0);
 
-            // Scheduler Decision (RAM/Length Ratio + Deadline)
-            
-            
+            // 6. Schedule cloudlets based on RAM/Length ratio and deadline
             for (Cloudlet c : cloudletList) {
                 int ram = ramMap.get(c.getCloudletId());
                 double deadline = deadlineMap.get(c.getCloudletId());
                 double ratio = (double) ram / c.getCloudletLength();
                 String decision = "CPU";
-                if (ratio > 0.004 && deadline > 30.0) {
-                    decision = "PIM";
-                }
+                if (ratio > 0.004 && deadline > 30.0) decision = "PIM";
 
-                if (decision.equals("PIM")) {
-                    c.setVmId(pimVm.getId());
-                } else {
-                    c.setVmId(cpuVm.getId());
+                // Find the first available matching VM
+                for (Vm vm : vmList) {
+                    boolean isPIM = vm.getMips() < 9000;
+                    if ((decision.equals("PIM") && isPIM) || (decision.equals("CPU") && !isPIM)) {
+                        c.setVmId(vm.getId());
+                        Log.printLine("Assigned Cloudlet " + c.getCloudletId() + " to VM " + vm.getId() + " (" + decision + ")");
+                        break;
+                    }
                 }
-
-                Log.printLine("Assigned Cloudlet " + c.getCloudletId() + " to VM " + c.getVmId() + " (" + decision + ")");
             }
 
-            // 7. Submit cloudlets after scheduling
+            // 7. Submit cloudlets after assigning VM IDs
             broker.submitCloudletList(cloudletList);
 
             // 8. Start simulation
             CloudSim.startSimulation();
             CloudSim.stopSimulation();
 
-            // 9. Print results
+            // 9. Display output
             List<Cloudlet> newList = broker.getCloudletReceivedList();
             printCloudletList(newList);
-
             Log.printLine("CloudSimExample1 finished!");
 
         } catch (Exception e) {
@@ -106,30 +96,29 @@ public class CloudSimExample1 {
 
     private static Datacenter createDatacenter(String name) throws Exception {
         List<Host> hostList = new ArrayList<>();
-        hostList.add(HeterogeneousHostConfig.createCPUHost(0));
-        hostList.add(HeterogeneousHostConfig.createPIMHost(1));
+        hostList.addAll(HeterogeneousHostConfig.generateMultipleCPUHosts());
+        hostList.addAll(HeterogeneousHostConfig.generateMultiplePIMHosts());
 
         DatacenterCharacteristics characteristics = new DatacenterCharacteristics(
-                "x86", "Linux", "Xen", hostList, 10.0, 3.0, 0.05, 0.001, 0.0);
+            "x86", "Linux", "Xen", hostList, 10.0, 3.0, 0.05, 0.001, 0.0
+        );
 
         return new Datacenter(name, characteristics, new VmAllocationPolicySimple(hostList), new LinkedList<>(), 0.1);
     }
 
     private static void printCloudletList(List<Cloudlet> list) {
         String indent = "    ";
-        Log.printLine("\n========== OUTPUT ==========");
-        Log.printLine("Cloudlet ID" + indent + "STATUS" + indent + "Data Center ID" + indent + "VM ID" + indent +
-                "Time" + indent + "Start Time" + indent + "Finish Time");
-
         DecimalFormat dft = new DecimalFormat("###.##");
+        Log.printLine("\n========== OUTPUT ==========");
+        Log.printLine("Cloudlet ID" + indent + "STATUS" + indent + "Data Center ID" + indent + "VM ID" +
+                      indent + "Time" + indent + "Start Time" + indent + "Finish Time");
 
-        for (Cloudlet cloudlet : list) {
-            Log.print(indent + cloudlet.getCloudletId() + indent);
-            if (cloudlet.getCloudletStatus() == Cloudlet.SUCCESS) {
-                Log.print("SUCCESS" + indent + indent + cloudlet.getResourceId() + indent + indent +
-                        cloudlet.getVmId() + indent + indent + dft.format(cloudlet.getActualCPUTime()) +
-                        indent + indent + dft.format(cloudlet.getExecStartTime()) + indent +
-                        indent + dft.format(cloudlet.getFinishTime()));
+        for (Cloudlet c : list) {
+            Log.print(indent + c.getCloudletId() + indent);
+            if (c.getCloudletStatus() == Cloudlet.SUCCESS) {
+                Log.print("SUCCESS" + indent + indent + c.getResourceId() + indent + indent + c.getVmId()
+                          + indent + indent + dft.format(c.getActualCPUTime()) + indent + indent
+                          + dft.format(c.getExecStartTime()) + indent + indent + dft.format(c.getFinishTime()));
             }
             Log.printLine();
         }
--- CloudSim_PIM_Scheduler_project/sources/pimsim/HeterogeneousHostConfig.java
+++ CloudSim_PIM_Scheduler_project/sources/pimsim/HeterogeneousHostConfig.java
@@ -1,34 +1,23 @@
 package pimsim;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.cloudbus.cloudsim.Host;
-import org.cloudbus.cloudsim.Pe;
-import org.cloudbus.cloudsim.VmSchedulerTimeShared;
-import org.cloudbus.cloudsim.power.models.PowerModel;
-import org.cloudbus.cloudsim.power.models.PowerModelLinear;
-import org.cloudbus.cloudsim.power.PowerHost;
-import org.cloudbus.cloudsim.provisioners.BwProvisionerSimple;
-import org.cloudbus.cloudsim.provisioners.PeProvisionerSimple;
-import org.cloudbus.cloudsim.provisioners.RamProvisionerSimple;
-
+import java.util.*;
 import org.cloudbus.cloudsim.*;
 import org.cloudbus.cloudsim.power.*;
-
-import java.util.*;
+import org.cloudbus.cloudsim.power.models.PowerModel;
+import org.cloudbus.cloudsim.power.models.PowerModelLinear;
+import org.cloudbus.cloudsim.provisioners.*;
 
 public class HeterogeneousHostConfig {
 
-    public static Host createCPUHost(int id) {
+    // Modified method to create a CPU Host with configurable MIPS and RAM
+    public static Host createCPUHost(int id, int mips, int ram) {
         List<Pe> peList = new ArrayList<>();
-        peList.add(new Pe(0, new PeProvisionerSimple(10000))); // 10,000 MIPS
+        peList.add(new Pe(0, new PeProvisionerSimple(mips))); // PE with configurable MIPS
 
-        int ram = 16384; // 16 GB
         long storage = 1_000_000; // 1 TB
         int bw = 10000;
 
-        PowerModel powerModel = new PowerModelLinear(200, 250); // CPU = high power
+        PowerModel powerModel = new PowerModelLinear(200, 250); // High power for CPU
 
         return new PowerHost(
             id,
@@ -41,15 +30,15 @@ public class HeterogeneousHostConfig {
         );
     }
 
-    public static Host createPIMHost(int id) {
+    // Modified method to create a PIM Host with configurable MIPS and RAM
+    public static Host createPIMHost(int id, int mips, int ram) {
         List<Pe> peList = new ArrayList<>();
-        peList.add(new Pe(0, new PeProvisionerSimple(8000))); // 8,000 MIPS
+        peList.add(new Pe(0, new PeProvisionerSimple(mips))); // PE with configurable MIPS
 
-        int ram = 32768; // 32 GB
-        long storage = 1_000_000;
+        long storage = 1_000_000; // 1 TB
         int bw = 10000;
 
-        PowerModel powerModel = new PowerModelLinear(80, 120); // PIM = low power
+        PowerModel powerModel = new PowerModelLinear(80, 120); // Lower power for PIM
 
         return new PowerHost(
             id,
@@ -60,5 +49,23 @@ public class HeterogeneousHostConfig {
             new VmSchedulerTimeShared(peList),
             powerModel
         );
+    }
+
+    // Helper: Generate a list of CPU Hosts with varying configurations
+    public static List<Host> generateMultipleCPUHosts() {
+        List<Host> cpuHosts = new ArrayList<>();
+        cpuHosts.add(createCPUHost(0, 10000, 16384)); // High-end
+        cpuHosts.add(createCPUHost(1, 8000, 8192));   // Mid-tier
+        cpuHosts.add(createCPUHost(2, 6000, 4096));   // Low-tier
+        return cpuHosts;
+    }
+
+    // Helper: Generate a list of PIM Hosts with varying configurations
+    public static List<Host> generateMultiplePIMHosts() {
+        List<Host> pimHosts = new ArrayList<>();
+        pimHosts.add(createPIMHost(3, 8000, 32768));  // High-end PIM
+        pimHosts.add(createPIMHost(4, 6000, 16384));  // Mid-tier
+        pimHosts.add(createPIMHost(5, 4000, 8192));   // Low-tier
+        return pimHosts;
     }
 }
